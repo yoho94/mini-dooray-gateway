b6bd9a23946d214deed054d39c7e5f0f09740d26
kimhyunjun <rlaguswns94@naver.com>
2023-06-01 20:10:52 +0900

    init

---
+++ src/main/java/com/nhn/minidooray/gateway/MiniDoorayGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.minidooray.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDoorayGatewayApplication.class, args);
+    }
+
+}
